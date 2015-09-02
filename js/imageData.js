c911b2a7783f22657b030852af37d636dc223b43
BBBrandon <langzulaoyishi@163.com>
2015-09-02 12:50:13 +0800

    首次提交

---
+++ js/imageData.js
@@ -0,0 +1,9 @@
+var imageInfo = [
+  {h2:'sudu',h3:'lutian o me',imageSrc:'img/1'},
+  {h2:'sudu',h3:'lutian o me',imageSrc:'img/2'},
+  {h2:'sudu',h3:'lutian o me',imageSrc:'img/3'},
+  {h2:'sudu',h3:'lutian o me',imageSrc:'img/4'},
+  {h2:'sudu',h3:'lutian o me',imageSrc:'img/5'},
+  {h2:'sudu',h3:'lutian o me',imageSrc:'img/6'},
+  {h2:'sudu',h3:'lutian o me',imageSrc:'img/7'}
+];
